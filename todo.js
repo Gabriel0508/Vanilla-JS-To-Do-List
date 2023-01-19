258ffe68d8a6aee359a89de24e2f616ad8f14297
Gabriel Grad <65502252+Gabriel0508@users.noreply.github.com>
2023-01-19 21:37:51 +0200

    - added bootstrap - added font-awesome - created the content - added functionality to add a new item

--- todo.js
+++ todo.js
@@ -1,3 +1,29 @@
-"use strict"
+"use strict";
 
-console.log("Works");
+const addNewItem = () => {
+  const newItem = document.getElementById("new-item");
+  const items = document.getElementById("items-list");
+  const KEY_ENTER = 13;
+
+  newItem.addEventListener("keypress", (event) => {
+    //event.preventDefault();
+    const itemText = newItem.value;
+
+    if (event.keyCode === KEY_ENTER) {
+      if (itemText === "") {
+        alert("BUUUHHHH");
+      }
+      const itemElement = document.createElement("light");
+      itemElement.appendChild(document.createTextNode(itemText));
+
+      const liItemElement = document.createElement("li");
+      liItemElement.appendChild(itemElement);
+      liItemElement.classList.add("todo-items");
+      liItemElement.style.marginTop = ".5rem";
+      liItemElement.style.border = ".1rem solid red";
+      items.appendChild(liItemElement);
+    }
+  });
+};
+
+addNewItem();
