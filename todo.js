b78402977bb0002d03ca7ceb5843087f292acb99
Gabriel Grad <65502252+Gabriel0508@users.noreply.github.com>
2023-01-20 22:06:59 +0200

    - added variables - added toggle/error functionality

--- todo.js
+++ todo.js
@@ -3,27 +3,51 @@
 const addNewItem = () => {
   const newItem = document.getElementById("new-item");
   const items = document.getElementById("items-list");
+  const error = document.getElementById("error-text");
+  const itemsToggle = document.getElementById("toggle-items");
   const KEY_ENTER = 13;
 
   newItem.addEventListener("keypress", (event) => {
     //event.preventDefault();
     const itemText = newItem.value;
-
+    
     if (event.keyCode === KEY_ENTER) {
       if (itemText === "") {
-        alert("BUUUHHHH");
+        newItem.classList.add("input-error");
+        error.style.display = "flex";
+        return;
       }
-      const itemElement = document.createElement("light");
+
+      const itemElement = document.createElement("strong");
       itemElement.appendChild(document.createTextNode(itemText));
 
       const liItemElement = document.createElement("li");
       liItemElement.appendChild(itemElement);
+      //liItemElement.classList.add("fa-xmark")
       liItemElement.classList.add("todo-items");
-      liItemElement.style.marginTop = ".5rem";
-      liItemElement.style.border = ".1rem solid red";
-      items.appendChild(liItemElement);
+      liItemElement.classList.add("li-item");
+      items.prepend(liItemElement);
+
+      newItem.value = ""; 
     }
   });
+
+  itemsToggle.addEventListener("click", () => {
+    /**
+     * TODO: toggle just when the list contains min. one li item
+     */
+    if (items !== 0) {
+      itemsToggle.classList.add("fa-angle-double-up")
+      itemsToggle.classList.remove("fa-angle-double-down")
+      items.classList.toggle("toggle-all");
+    }
+  })
+
+  //TODO: remove the error message on adding char in input
+  newItem.addEventListener("change", (event) => {
+    newItem.classList.remove("input-error");
+    error.style.display = "none";
+  })
 };
 
 addNewItem();
