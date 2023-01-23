a16060e40531be386f494284be113b495eefdfd6
Gabriel Grad <65502252+Gabriel0508@users.noreply.github.com>
2023-01-23 23:10:03 +0200

    - added remove functionality

--- todo.js
+++ todo.js
@@ -5,42 +5,58 @@ const addNewItem = () => {
   const items = document.getElementById("items-list");
   const error = document.getElementById("error-text");
   const itemsToggle = document.getElementById("toggle-items");
+  const countItems = document.getElementsByClassName("todo-count");
   const KEY_ENTER = 13;
 
   newItem.addEventListener("keypress", (event) => {
     //event.preventDefault();
     const itemText = newItem.value;
     const liItemsElement = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.classList.add("delete-btn");
 
     if (event.keyCode === KEY_ENTER) {
       if (itemText === "") {
         newItem.classList.add("input-error");
         error.style.display = "flex";
+        error.innerHTML = "Add a item that needs to be done!";
         return;
       } else {
         newItem.classList.remove("input-error");
         error.style.display = "none";
+      
+      //  countItems.style.display = "flex";
       }
 
       const itemElement = document.createTextNode(itemText);
       liItemsElement.appendChild(itemElement);
-      //liItemsElement.classList.add("fa-solid-fa-xmark")
+      liItemsElement.appendChild(deleteBtn);
       liItemsElement.classList.add("todo-items");
       liItemsElement.classList.add("li-item");
       items.prepend(liItemsElement);
       newItem.value = "";
     }
 
+    //Toggle the todos items
     itemsToggle.addEventListener("click", () => {
-        if (items !== "") {
-          itemsToggle.classList.add("fa-angle-double-up");
-          itemsToggle.classList.remove("fa-angle-double-down");
-          items.classList.toggle("toggle-all");
-        } else {
-          itemsToggle.classList.remove("fa-angle-double-up");
-          itemsToggle.classList.add("fa-angle-double-down");
-        }
+      if (items.contains(liItemsElement)) {
+        items.classList.toggle("toggle-all");
+      }
+      return;
+    });
+
+    //Delete a todos item
+    deleteBtn.addEventListener("click", () => {
+      liItemsElement.remove();
+    });
+
+    //Count the todos items
+    window.addEventListener("load", () => {
+      countItems.innerText = liItemsElement.lang;
+      console.log(countItems);
     });
   });
 };
+
 addNewItem();
